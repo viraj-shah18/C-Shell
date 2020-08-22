52e130208f0efd640662005b6304e6b49e9f463a
Viraj Shah <viraj0605@gmail.com>
2020-08-22 19:53:15 +0530

    some minor changes and added comments.

--- main.c
+++ main.c
@@ -8,21 +8,20 @@
 
 #define SHELL_NAME "fish"
 #define MAX_LEN_CMD 128
-#define BASE_PATH_BIN "/usr/bin/"
 
+// function declarations
 int read_input(char *, char*[]);
 
 int main(){
-
-
     while (1){
-        printf("%s>", SHELL_NAME);
+        printf("%s> ", SHELL_NAME);
         int input_size;
         char *input_str[MAX_LEN_CMD];
         char *parsed_input[MAX_LEN_CMD];
 
         input_size = read_input(&input_str, parsed_input);
 
+        // this is to slice the array to required input length and adding NULL as last element
         char *input_array[input_size+1];
         for (int idx=0;idx<input_size;idx++){
             input_array[idx]=parsed_input[idx];
@@ -31,18 +30,15 @@ int main(){
 
         int rc=fork();
         if (rc<0){
-            printf("Main Fork failed\n");
+            printf("Function main-Fork failed\n");
             return 0;
         }
         else if(rc==0){
-            char *function_call=input_array[0];
-            // printf("%s", function_call);
-            // printf("%d", input_size);
-            int ec = execvp(function_call, input_array);
+            char *command=input_array[0];
+            int ec = execvp(command, input_array);
             if (ec<0){
                 printf("Command not known. Please enter valid command\nExec failed\n");
             }
-            // printf("NOT printed\n");
         }
         else{
             wait(NULL);
@@ -54,13 +50,15 @@ int main(){
 int read_input(char *input_str, char *parsed_input[]){
     /*
     This function reads input and splits them on spaces using strtok function
+    and returns the length of array created 
     */
     const char delim[]=" ";
     char *token;
 
     fgets(input_str, MAX_LEN_CMD, stdin);
-    strtok(input_str, "\n");
+    strtok(input_str, "\n");    // this is to remove the last "\n" character from the input
 
+    // tokenising the input using " " as delimiter
     token = strtok(input_str, delim);
     int input_size=0;
 
@@ -69,5 +67,6 @@ int read_input(char *input_str, char *parsed_input[]){
         token=strtok(NULL, delim);
         input_size++;
     }
+
     return input_size;
 }
